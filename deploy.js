50bf17987f4a184b47582c99128576b4fdae62e5
Gauthier Petetin <gauthierpetetin@hotmail.com>
2018-03-29 18:42:48 +0200

    Removal of strict inequality for minimum contribution

--- deploy.js
+++ deploy.js
@@ -3,15 +3,25 @@ const HDWalletProvider = require('truffle-hdwallet-provider');
 const Web3 = require('web3');
 const {interface, bytecode} = require('./compile');
 
+// Rinkeby
 const provider = new HDWalletProvider(
   'come ocean involve battle onion dinosaur cream oppose axis chimney size pet', //Mnemonic
   'https://rinkeby.infura.io/EUpGstKhtEWq2rhdBxbQ'
 );
+// FidEver
+// const provider = new HDWalletProvider(
+//   'come ocean involve battle onion dinosaur cream oppose axis chimney size pet', //Mnemonic
+//   'http://fidever.io:8545'
+// );
+
+// const provider = new Web3.providers.HttpProvider('https://rinkeby.infura.io/EUpGstKhtEWq2rhdBxbQ')
+
 const web3 = new Web3(provider);
 
 // We create a function only to use async
 const deploy = async () => {
   const accounts = await web3.eth.getAccounts();
+  console.log('Accounts ', accounts);
   console.log('Attempting to deploy from account ', accounts[0]);
   const result = await new web3.eth.Contract(JSON.parse(interface))
     .deploy({ data: bytecode, arguments: '0' })
