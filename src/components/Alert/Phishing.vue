da5d104937b7f383ca0590e436489e1edde6b69b
Gauthier Petetin <gauthierpetetin@hotmail.com>
2018-05-15 18:30:04 +0200

    Phishing alert

--- src/components/Alert/Phishing.vue
+++ src/components/Alert/Phishing.vue
@@ -1,6 +1,7 @@
 <template>
-  <v-alert v-model="alert" type="info" dismissible>
-    IMPORTANT! DON'T GET PHISHED, please check that you are visiting the right url https://piggybreaker.com/
+  <v-alert v-model="alert" type="info" dismissible style="padding-top: 10px; padding-bottom: 10px">
+    IMPORTANT! Please check you're on the right url: https://piggybreaker.com/
+    <img src="/static/img/icon/arrow.png" height="20" style="margin-left: 15px">
   </v-alert>
 </template>
 
