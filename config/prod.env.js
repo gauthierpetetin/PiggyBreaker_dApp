59601a0fbbdc0644d647da9e915b1580edbab3bb
Raphaël <raphael.pralat@gmail.com>
2018-05-21 23:03:00 +0200

    firestore credentials for dev and prod

--- config/prod.env.js
+++ config/prod.env.js
@@ -3,5 +3,13 @@ module.exports = {
   NODE_ENV: '"production"',
   API_URL: '"http://fidever.io:8080/api"',
   ETHEREUM_NODE_ENV: '"development"',
-  ETHEREUM_NODE_URL: '"wss://ropsten.eth.6120.eu/ws"'
+  ETHEREUM_NODE_URL: '"wss://ropsten.eth.6120.eu/ws"',
+  FIRESTORE_APIKEY: JSON.stringify({
+    apiKey: "AIzaSyDHHJ3_3xPBexOicUlsfeXUbhIjnGfX2-g",
+    authDomain: "piggy-breaker.firebaseapp.com",
+    databaseURL: "https://piggy-breaker.firebaseio.com",
+    projectId: "piggy-breaker",
+    storageBucket: "piggy-breaker.appspot.com",
+    messagingSenderId: "416246154436"
+  })
 }
