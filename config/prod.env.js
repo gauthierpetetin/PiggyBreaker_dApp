d28d9f43573fed1b2b107feef582aeaf219446ff
Raphaël <raphael.pralat@gmail.com>
2018-05-25 17:10:56 +0200

    firestore credentials

--- config/prod.env.js
+++ config/prod.env.js
@@ -4,12 +4,20 @@ module.exports = {
   API_URL: '"http://fidever.io:8080/api"',
   ETHEREUM_NODE_ENV: '"development"',
   ETHEREUM_NODE_URL: '"wss://ropsten.eth.6120.eu/ws"',
+  // FIRESTORE_APIKEY: JSON.stringify({
+  //   apiKey: "AIzaSyDHHJ3_3xPBexOicUlsfeXUbhIjnGfX2-g",
+  //   authDomain: "piggy-breaker.firebaseapp.com",
+  //   databaseURL: "https://piggy-breaker.firebaseio.com",
+  //   projectId: "piggy-breaker",
+  //   storageBucket: "piggy-breaker.appspot.com",
+  //   messagingSenderId: "416246154436"
+  // })
   FIRESTORE_APIKEY: JSON.stringify({
-    apiKey: "AIzaSyDHHJ3_3xPBexOicUlsfeXUbhIjnGfX2-g",
-    authDomain: "piggy-breaker.firebaseapp.com",
-    databaseURL: "https://piggy-breaker.firebaseio.com",
-    projectId: "piggy-breaker",
-    storageBucket: "piggy-breaker.appspot.com",
-    messagingSenderId: "416246154436"
+    apiKey: 'AIzaSyAYWbp_eZO_gZnyiKUvcULbELP0kl6z_Xg',
+    authDomain: 'piggybreaker-dev.firebaseapp.com',
+    databaseURL: 'https://piggybreaker-dev.firebaseio.com',
+    projectId: 'piggybreaker-dev',
+    storageBucket: 'piggybreaker-dev.appspot.com',
+    messagingSenderId: '531395449330'
   })
 }
