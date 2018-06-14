d35d34fa1bee3ef2de7c3e638a8ef9145b87fb6e
Gauthier Petetin <gauthierpetetin@hotmail.com>
2018-06-14 16:20:39 +0200

    Version

--- src/components/Footer/Footer.vue
+++ src/components/Footer/Footer.vue
@@ -1,14 +1,21 @@
 <template>
   <v-footer style="background-color: #404040">
     <v-layout row wrap align-center>
-      <v-flex xs6>
+      <v-flex xs1>
+      </v-flex>
+      <v-flex xs5>
         <div class="black--text ml-3 text-xs-right">
           <router-link class="white--text" to="terms-of-use">Terms of use</router-link>
         </div>
       </v-flex>
-      <v-flex xs6>
+      <v-flex xs5>
         <div class="black--text ml-3 text-xs-left">
           <router-link class="white--text" to="privacy-policy">Privacy policy</router-link>
+        </div>
+      </v-flex>
+      <v-flex xs1>
+        <div class="black--text ml-3 text-xs-left">
+          <p class="white--text" style="margin: 5px 5px;">v.0.9.1</p>
         </div>
       </v-flex>
     </v-layout>
